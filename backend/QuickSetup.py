6660358932a6a97d0a332fb04346f0caa3034058
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-27 15:42:43 -0400

    added get process

--- backend/QuickSetup.py
+++ backend/QuickSetup.py
@@ -1,18 +1,25 @@
 import webapp2
 import datetime
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 
-class Script(db.Model):
-	route = db.StringProperty(required=True)
-	script = db.StringProperty(required=True)
-	runs = db.IntegerProperty(required=True)
-	creation_date = db.DateProperty(required=True)
+class Script(ndb.Model):
+	route = ndb.StringProperty(required=True)
+	script = ndb.StringProperty(required=True)
+	runs = ndb.IntegerProperty(required=True)
+	creation_date = ndb.DateProperty(auto_now_add=True)
 
 class Reserve(webapp2.RequestHandler):
 	def post(self):
+		route = self.request.path
+		script = self.request.get("script")
+		reservation = Script(route=route, script=script, runs=0)
+		reservation.put()
+
+class Retrieve(webapp2.RequestHandler):
+	def get(self):
 		print self.request.path
-		print self.request.get("script")
 
 app = webapp2.WSGIApplication([
-	('/.*', Reserve),
+	('/reserve/.*', Reserve),
+	('/.*', Retrieve)
 ], debug=True)
