b479ed2aad3f4250ae9c31ec62284e817fd738c9
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-27 14:09:17 -0400

    there can be no other requests to the server besides /, /reserve, and the scripts that we want to host

--- backend/QuickSetup.py
+++ backend/QuickSetup.py
@@ -1,9 +1,18 @@
 import webapp2
+import datetime
+from google.appengine.ext import db
+
+class Script(db.Model):
+	route = db.StringProperty(required=True)
+	script = db.StringProperty(required=True)
+	runs = db.IntegerProperty(required=True)
+	creation_date = db.DateProperty(required=True)
 
 class Reserve(webapp2.RequestHandler):
-    def post(self):
-        print self.request.path
+	def post(self):
+		print self.request.path
+		print self.request.get("script")
 
 app = webapp2.WSGIApplication([
-    ('/.*', Reserve),
+	('/.*', Reserve),
 ], debug=True)
